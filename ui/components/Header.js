3282098e17e00701ef266e1e4a836eb9b75b2cd3
Tyler Burnam <tyler@burnamtech.com>
2019-10-04 11:26:14 -0700

    updated styling

--- ui/components/Header.js
+++ ui/components/Header.js
@@ -9,7 +9,10 @@ export default function Header() {
       id="header"
       style={{ boxShadow: scrolled ? '0 3px 3px rgba(0,0,0,0.1)' : 'none' }}
     >
-      <span>GT Transaction Queue Dashboard</span>
+      <div className="navLogo" style={{display: 'flex', flexFlow: 'column'}}>
+        <img style={{width: '8%', paddingTop: '20px'}} src='https://landen.imgix.net/92clds4bviei/assets/41e7ytvs.png?w=400' />
+        <span style={{lineHeight: '35px'}}>Transaction Queue Dashboard</span>
+      </div>
     </nav>
   )
 }
